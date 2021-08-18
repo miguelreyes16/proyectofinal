0dc39dc74805c4b4aca4baa253a6ab991a9b12d8
miguelreyes16 <78574021+miguelreyes16@users.noreply.github.com>
2021-08-18 13:37:21 -0400

    Add files via upload

---
+++ includes/cabeza-admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="includes\css.css">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-warning">
+        <div class="container">
+                <a class="navbar-brand" href="../admin/index-admin.php"> Camiones Dorlex </a>
+           
+                <a class="navbar-brand" href="../admin/lavadoras.php"> Lavadoras </a>
+                <a class="navbar-brand" href="../admin/camiones.php">Camiones</a>
+                <button type="button" id=""  onclick="window.location='../admin/cerrar.php'" class="btn btn-danger btn-sm" width="30" height="30"> Cerrar Sesión</button>
+</nav>
---
+++ includes/db.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$database = "tarea";
+$username = "miguel";
+$password = "miguel";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+  
+
+    if( !$conn)
+
+{
+
+    echo("no hay na");
+
+
+}
+
+
+?>
